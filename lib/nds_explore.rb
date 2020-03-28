89ec326db263adc8c83a86875602c6c8720fa350
Darrick Pang <dkypang@yahoo.com>
2020-03-28 01:18:32 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,10 +9,7 @@ def pretty_print_nds(nds)
   movies= ["Close Encounters of the Third Kind","Close Encounters of the Third Kind", "the Extra-terrestrial list "  ]
   a_o_a= [[1,2,3],[456],[7,8,9]]
 end	
-  pp name
-  pp movies
-  pp a_o_a
-
+  
 
 
 def print_first_directors_movie_titles	
