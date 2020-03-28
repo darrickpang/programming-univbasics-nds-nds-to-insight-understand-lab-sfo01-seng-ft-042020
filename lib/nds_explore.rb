a9004b8c0af44f6d461fff2964cb080538f3d0fd
Darrick Pang <dkypang@yahoo.com>
2020-03-28 01:20:43 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,6 +8,7 @@ def pretty_print_nds(nds)
   names=["Jaws", "E.T","Schindler"]
   movies= ["Close Encounters of the Third Kind","Close Encounters of the Third Kind", "the Extra-terrestrial list "  ]
   a_o_a= [[1,2,3],[456],[7,8,9]]
+  pp nds
 end	
   
 
