7e93671102cde5f622062ebf4763ebae810ce7fa
Darrick Pang <dkypang@yahoo.com>
2020-03-28 01:18:09 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -15,7 +15,7 @@ end
 
 
 
-def print_first_directors_movie_titles	def print_first_directors_movie_titles
+def print_first_directors_movie_titles	
  movies=[{:name =>"Jaws", :movie =>"Close Encounters of the Third Kind"},
  {:name => "E.T", :movie => "Raiders of the Lost Ark"},
  {:name => "Schindler", :movie => " the Extra-terrestrial list "}]
