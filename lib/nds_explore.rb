0eeb8508902d824073009f6a90664a9698c93a8e
Darrick Pang <dkypang@yahoo.com>
2020-03-28 01:16:39 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,15 +3,20 @@ require 'pp'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
-  pp nds
-end
+  # Change the code below to pretty print the nds with pp	  # Change the code below to pretty print the nds with pp
+  nil	  nil
+  names=["Jaws", "E.T","Schindler"]
+  movies= ["Close Encounters of the Third Kind","Close Encounters of the Third Kind", "the Extra-terrestrial list "  ]
+  a_o_a= [[1,2,3],[456],[7,8,9]]
+end	end
+  pp name
+  pp movies
+  pp a_o_a
 
-def print_first_directors_movie_titles
-  x = 0 
-  movie = directors_database[0][:movies]
-  while x < movie.length
-    puts movie[x][:titles]
-    x += 1
-  end 
-end
+
+
+def print_first_directors_movie_titles	def print_first_directors_movie_titles
+ movies=[{:name =>"Jaws", :movie =>"Close Encounters of the Third Kind"},
+ {:name => "E.T", :movie => "Raiders of the Lost Ark"},
+ {:name => "Schindler", :movie => " the Extra-terrestrial list "}]
+end	end
